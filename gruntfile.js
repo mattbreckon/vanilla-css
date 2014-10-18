6e553beb11af6c87c7add4cf94ac02689645c6dc
Matt Breckon <matt@mattbreckon.com>
2014-10-18 17:58:18 -0400

    Update vanilla css

--- gruntfile.js
+++ gruntfile.js
@@ -1,75 +1,41 @@
 module.exports = function(grunt) {
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: [
+          'build/js/main.js'
+        ],
+        dest: 'build/js/scripts.js'
+      }
+    },
 
-        htmlhint: {
-            build: {
-                options: {
-                    'tag-pair': true,
-                    'tagname-lowercase': true,
-                    'attr-lowercase': true,
-                    'attr-value-double-quotes': true,
-                    'doctype-first': true,
-                    'spec-char-escape': true,
-                    'id-unique': true,
-                    'style-disabled': true
-                },
-                src: ['index.html']
-            }
-        },
+    uglify: {
+      build: {
+        src: 'build/js/scripts.js',
+        dest: 'assets/js/scripts.min.js'
+      }
+    },
 
-        concat: {
-            dist: {
-                src: [
-                    'build/js/main.js'
-                ],
-                dest: 'build/js/scripts.js'
-            }
-        },
-
-        uglify: {
-            build: {
-                src: 'build/js/scripts.js',
-                dest: 'assets/js/scripts.min.js'
-            }
-        },
-
-        sass: {
-            build: {
-                files: {
-                    'assets/css/style.css': 'build/css/main.scss'
-                }
-            }
-        },
-
-        cssmin: {
-            build: {
-                src: 'build/css/style.css',
-                dest: 'assets/css/style.min.css'
-            }
-        },
-
-        watch: {
-            html: {
-                files: ['index.html'],
-                tasks: ['htmlhint']
-            },
-
-            css: {
-                files: ['build/css/*.scss'],
-                tasks: ['sass']
-            }/*,
-            js: {
-                files: ['build/js/*.js'],
-                tasks: ['concat']
-            }
-            //*/
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'build/css',
+          cssDir: 'assets/css'
         }
-    });
+      }
+    },
 
-    grunt.registerTask('default', []);
+    watch: {
+      css: {
+        files: ['build/css/*.scss'],
+        tasks: ['compass']
+      }
+    }
+  });
 
+  grunt.registerTask('default', []);
 };
